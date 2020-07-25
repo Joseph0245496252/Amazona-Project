86907be602251683ed344118f7f12fba065b285d
Joseph Asante <freecoding1@gmail.com>
2020-07-25 19:51:00 +0000

    Amazona Project dynamically install

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export default {
+    products:[
+        {
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image: 'images/d1.jpeg', 
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: '10 Reviews'
+    },
+    {
+        name: 'Fit Shirt',
+        category: 'Shirts',
+        image: 'images/d1.jpeg',
+        price: 50,
+        brand: 'Nike',
+        rating: 4.2,
+        numReviews: '8 Reviews'
+    },
+    {
+        name: 'Best Pants',
+        category: 'Pants',
+        image: 'images/d1.jpeg',
+        price: 70,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: '7 Revews'
+    },
+    {
+        name: 'Best Pants',
+        category: 'Pants',
+        image: 'images/d1.jpeg',
+        price: 70,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: '7 Revews'
+    },
+
+]
+}
